b7f9dc45cafaba8356bed506440708c8167c39a7
Kristine Kochie <kristinekochie@gmail.com>
2022-07-01 20:10:11 -0400

    commit tree 2afe5521cabb6de0d69b6cb3784c89435cf82709

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -6,11 +6,30 @@ function SpicyFoodList() {
 
   function handleAddFood() {
     const newFood = getNewSpicyFood();
-    console.log(newFood);
+    // console.log(newFood);
+    const newFoodArray = [...foods, newFood]
+    setFoods(newFoodArray)
+  }
+
+  function handleLiClick(id) {
+    //To remove element from array in state
+    // const newFoodArray = foods.filter((food) => food.id !== id);
+
+    //To change one element of an array
+    //For ex, increase heat index of one food
+    const newFoodArray = foods.map((food) => {
+      if (food.id === id) {
+        return { ...food, heatLevel: food.heatLevel + 1, };
+      } else {
+        return food;
+      }
+    });
+
+    setFoods(newFoodArray);
   }
 
   const foodList = foods.map((food) => (
-    <li key={food.id}>
+    <li key={food.id} onClick={() => handleLiClick(food.id)}>
       {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
     </li>
   ));
